48b0bb366612b5eed63131a0907bf16c7f3fa8bd
Marcio Miranda <marcio.r.miranda@gmail.com>
2019-05-27 04:12:43 -0300

    Initial commit

---
+++ app/src/main/java/com/example/desenvolvimentomobile_nac1/AdicionaContato.kt
@@ -0,0 +1,19 @@
+/*
+Desenvolvimento Mobile - NAC1 - 5ECS
+RM 74409 - Ana Flavia Carneiro
+RM 74407 - Fabio Hellu
+RM 73769 - Marcio Miranda
+RM 75164 - Ricardo Rodrigues Silva
+ */
+package com.example.desenvolvimentomobile_nac1
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class AdicionaContato : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_adiciona_contato)
+    }
+}
